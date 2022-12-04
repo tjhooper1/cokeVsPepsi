000184e6284d11f95f3208b57e9f551302dc6f20
tjhooper1 <tomh386@gmail.com>
2022-12-03 23:20:02 -0500

    added polling

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
 	return (
 		<div className="h-screen bg-white bg-gradient-to-r from-coke to-pepsi">
 			<header className="flex items-center justify-center">
-				<span className="m-3 text-4xl font-bold text-red-400">
+				<span className="m-3 text-6xl font-bold text-red-400">
 					Coke
 				</span>
-				<span className="m-3 text-2xl text-white">Vs</span>
-				<span className="m-3 text-4xl font-bold text-blue-400">
+				<span className="m-3 text-4xl text-white">Vs</span>
+				<span className="m-3 text-6xl font-bold text-blue-400">
 					Pepsi
 				</span>
 			</header>
@@ -32,7 +32,8 @@ function App() {
 					</div>
 				</section>
 				<section>
-					<PollResults name="Coke" />
+					<PollResults name="Coke" percentage={70} />
+					<PollResults name="Pepsi" percentage={40} />
 				</section>
 			</main>
 			<footer></footer>
--- src/components/PollResults.tsx
+++ src/components/PollResults.tsx
@@ -1,13 +1,21 @@
-// poll container
-
 type PollResultsType = {
 	name: string;
+	percentage: number;
 };
 
-export default function PollResults({ name }: PollResultsType) {
+export default function PollResults({ name, percentage }: PollResultsType) {
+	const color = name === 'Coke' ? 'red' : 'blue';
+	const barWidth = 'w-[' + percentage.toString() + '%]';
+
 	return (
-		<div className="m-auto mt-6 flex w-10/12 items-center justify-between">
-			<div className=" h-3 w-3/4 rounded-xl bg-gray-200" />
+		<div className="m-auto mt-6 mb-7 flex w-10/12 items-center justify-between">
+			<div className=" h-6 w-3/4 rounded-xl border-none bg-gray-300">
+				<div className={`h-6 ${barWidth} rounded-xl bg-${color}-700`} />
+				<div className="flex justify-center gap-3">
+					<p className="text-center  text-white">100 votes</p>
+					<p className="text-center text-white">40%</p>
+				</div>
+			</div>
 			<h1 className="text-center text-2xl font-bold text-white">
 				{name}
 			</h1>
